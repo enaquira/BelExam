eb812bad70110c7221520cf646364691ad2a8f9e
Eduardo Náquira <eduardo.naquira@gmail.com>
2018-04-06 10:58:46 -0500

    Creación del proyecto

---
+++ BelExamen/Models/Producto/ProductosModel.cs
@@ -0,0 +1,14 @@
+﻿using BelExamen.BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BelExamen.Models
+{
+    public class ProductosModel
+    {
+        public List<ProductoBE> Productos { get; set; }
+        public String NombreProducto { get; set; }
+    }
+}
